f48f23d1625143f7b4d32ff9d3d53e597ed213d8
Kevin Hyde <kphyde@gmail.com>
2023-08-07 14:59:05 -0700

    small content edit and footer position

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { h3Style } from '@/styles/tailwindStyles';
 
 function Footer() {
     return (
-        <footer className='w-full p-10 bg-slate-800'>
+        <footer className='w-full p-10 bg-slate-800 bottom-0'>
             <div className='inline-flex gap-4 items-center'>
                 <Link href={'/'}>
                     <h3 className={h3Style}>Kevin Hyde</h3>
