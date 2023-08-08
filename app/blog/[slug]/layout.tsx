7eaa491c7c0e4d6b7324f2b2769492ee574cc7f2
Kevin Hyde <kphyde@gmail.com>
2023-08-08 15:19:59 -0700

    Feat navigation (#8)
    * add top navigation with dynamic link paths, cleanup style applicaitons, style responsive widths
    
    * improve responsiveness for header nav
    
    * add resume link, fix details styles, improve homepage link styles
    
    * responsive flow for home page nav links

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Footer from "@/components/Footer";
 import styles from '@/styles/Home.module.css'
-import { backLinkStyle } from '@/styles/tailwindStyles';
+import { backLinkStyle, mainWidthStyles } from '@/styles/tailwindStyles';
+import Nav from '@/components/Nav';
 
 export default function RootLayout({
     children,
@@ -14,7 +15,10 @@ export default function RootLayout({
             <Head>
                 <title>Blog</title>
             </Head>
-            <main className={styles.main} >
+            <div className={`my-2 mx-auto p-2 ${mainWidthStyles} bg-slate-900 rounded-xl`}>
+                <Nav includeTitle/>
+            </div>
+            <main className={mainWidthStyles} >
                 {children}
                 <nav>
                     <Link className={backLinkStyle} href={'/blog'}>👈 Back</Link>
