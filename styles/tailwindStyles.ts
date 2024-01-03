d5114441dac76d3795528ddd518499b1fce58511
Kevin Hyde <kphyde@gmail.com>
2024-01-02 18:40:20 -0800

    hotfix nav color style and h2 in posts

--- styles/tailwindStyles.ts
+++ styles/tailwindStyles.ts
@@ -1,4 +1,5 @@
 export const h1Style = "sm:mb-6 text-5xl font-black text-yellow-300";
+export const h2Style = "text-4xl font-bold tracking-wide my-6 text-yellow-300";
 export const h3Style = "text-2xl font-bold tracking-wide my-6 text-yellow-300";
 export const detailTitleStyle =
   "w-1/2 py-2 px-4 bg-blue-700 radius-sm transition-all";
@@ -7,7 +8,7 @@ export const linkStyles =
 export const homeLinkStyle =
   "my-2 py-1 px-2 w-fit text-xs text-blue-150 hover:text-white font-[400] sm:text-sm border-solid border-2 border-transparent rounded-md hover:bg-blue-950 hover:border-b-blue-600 transition-all";
 export const backLinkStyle =
-  "py-2 sm:px-4 text-xs font-[400] bg-none sm:bg-slate-900 border-solid border-2 border-transparent rounded-md hover:bg-blue-950 hover:border-b-blue-600 transition-all";
+  "py-2 sm:px-4 text-xs font-[400] bg-none border-solid border-2 border-transparent rounded-md hover:bg-blue-950 hover:border-b-blue-600 transition-all";
 export const navTitleStyle =
   "leading-unset flex flex-.5 text-2xl font-bold text-yellow-300";
 export const mainWidthStyles = "p-4 md:p-8 w-11/12 lg:w-8/12 flex-1";
