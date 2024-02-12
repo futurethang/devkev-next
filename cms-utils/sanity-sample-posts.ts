08687840940f2d5f7a1ede00df5c6fe130b674f5
Kevin Hyde <kphyde@gmail.com>
2024-02-12 13:21:20 -0800

    Post list updates (#21)
    * Standardize post list between blog and work samples, make PostListCard its own component
    
    * list cards design tweaks, order update on samples Groq

--- cms-utils/sanity-sample-posts.ts
+++ cms-utils/sanity-sample-posts.ts
@@ -3,9 +3,10 @@ import { client } from "./sanity-client";
 
 export async function getSamplePosts() {
   return client.fetch(
-    groq`*[_type == "post" && references('45be0681-18a8-485c-95d3-325682e24033')]{
+    groq`*[_type == "post" && references('45be0681-18a8-485c-95d3-325682e24033')] | order(publishedAt desc) {
       _id,
       _createdAt,
+      publishedAt,
       slug,
       title,
       author,
@@ -15,6 +16,9 @@ export async function getSamplePosts() {
       image,
       mainImage,
       categories,
+      "tags": tags[]->{
+        title
+      }
       }
     `
   );
