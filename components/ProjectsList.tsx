7f0172535f821e60f26380bde303d686b920ab34
Kevin Hyde <kphyde@gmail.com>
2024-02-26 09:55:10 -0800

    Add Project board with filtering disabled

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { format } from "date-fns";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { Listbox, Transition } from "@headlessui/react";
 import { tagIcons } from "../constants/icons";
 import { h4Style } from "@/styles/tailwindStyles";
 
@@ -22,69 +24,126 @@ const statusIndicator = (status: string) => {
   }
 };
 
+function getTags(projects: any[]) {
+  const tags = projects.reduce((acc: any, project: any) => {
+    project.tags.forEach((tag: string) => {
+      acc[tag] = true;
+    });
+    return acc;
+  }, {});
+
+  return tags;
+}
+
 export default function ProjectsList({ projects }) {
   const [filteredProjects, setFilteredProjects] = useState(projects);
-  const [filter, setFilter] = useState("");
-  const [sort, setSort] = useState("dateUpdated");
+  // list of tags, never changes, just needs to be pulled from the projects
+  const [tags, setTags] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
 
-  useEffect(() => {
-    let newProjects = [...projects];
-    if (filter) {
-      newProjects = newProjects.filter((project) =>
-        project.tags.includes(filter)
-      );
-    }
-    newProjects.sort((a, b) => {
-      if (a[sort] < b[sort]) return -1;
-      if (a[sort] > b[sort]) return 1;
-      return 0;
-    });
-    setFilteredProjects(newProjects);
-  }, [filter, sort]);
+  // useEffect(() => {
+  //   setTags(getTags(projects));
+  // }, [projects]);
 
-  // Render the filtered and sorted projects
+  // useEffect(() => {
+  //   const newProjects = projects.filter((project) =>
+  //     selectedTags.some((tag) => project.tags.includes(tag))
+  //   );
+  //   setFilteredProjects(newProjects);
+  //   debugger;
+  // }, [projects, selectedTags]);
+
+  // const handleTagChange = (tag) => {
+  //   setSelectedTags((prevTags) =>
+  //     prevTags.includes(tag)
+  //       ? prevTags.filter((t) => t !== tag)
+  //       : [...prevTags, tag]
+  //   );
+  // };
+
+  // const handleSortChange = () => {
+  //   setFilteredProjects(projects);
+  // };
+
   return (
     <>
       <div className="flex mb-4">
-        <div>
-          <label htmlFor="filter">Filter by tag:</label>
-          <select
-            id="filter"
-            onChange={(e) => setFilter(e.target.value)}
-            value={filter}
-          >
-            <option value="">All</option>
-            {[
-              ...Array.from(
-                new Set(
-                  projects.reduce((acc, project) => {
-                    return [...acc, ...project.tags];
-                  }, [])
-                )
-              ),
-            ].map((tag, index) => (
-              <option key={index} value={tag}>
-                {tag}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="sort">Sort by:</label>
-          <select
-            id="sort"
-            onChange={(e) => setSort(e.target.value)}
-            value={sort}
-          >
-            <option value="dateUpdated">Date Updated</option>
-            <option value="status">Status</option>
-          </select>
-        </div>
+        {/* <div>
+          <Listbox value={filteredProjects} onChange={handleTagChange}>
+            <div className="relative mt-1">
+              <Listbox.Button className="relative flex flex-wrap w-full py-2 pl-3 pr-10 text-left bg-slate-800 rounded-lg cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <span className="flex flex-wrap gap-2 ">
+                  {Object.entries(tags).map(
+                    ([tag, value], index) =>
+                      value === true && (
+                        <span
+                          key={`${tag}-${index}`}
+                          className="bg-gray-400 rounded-full px-2 py-1"
+                        >
+                          {tag}
+                        </span>
+                      )
+                  )}
+                </span>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                  <FontAwesomeIcon icon={faChevronDown} aria-hidden="true" />
+                </span>
+              </Listbox.Button>
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-10 mt-1 w-full bg-slate-800 shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                  {Object.entries(tags).map(([tag, value], index) => (
+                    <Listbox.Option
+                      key={`${tag}-${index}`}
+                      className={({ active }) =>
+                        `${
+                          active
+                            ? "text-yellow-900 bg-yellow-100"
+                            : "text-white"
+                        }
+                          flex justify-between cursor-default select-none relative py-2 px-4`
+                      }
+                      value={tag}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <span
+                            className={`${
+                              selected ? "font-medium" : "font-normal"
+                            } block truncate`}
+                          >
+                            <FontAwesomeIcon
+                              icon={tagIcons[tag]}
+                              aria-hidden="true"
+                              aria-label={tag}
+                              className="w-4 mr-4"
+                            />
+                            {tag}
+                          </span>
+                          {value ? (
+                            <FontAwesomeIcon
+                              icon={faCheck}
+                              aria-hidden="true"
+                            />
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </Listbox>
+        </div> */}
       </div>
       <div className="flex justify-between">
-        {filteredProjects.length > 0 && (
+        {projects.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4  ">
-            {filteredProjects.map((project: any) => {
+            {projects.map((project: any) => {
               return (
                 <div
                   key={project.name}
@@ -117,6 +176,7 @@ export default function ProjectsList({ projects }) {
                               <FontAwesomeIcon
                                 icon={tagIcons[tag]}
                                 aria-hidden="true"
+                                aria-label={tag}
                               />
                               <span className="sr-only">{tag}</span>
                               <div className="tooltip">
@@ -140,7 +200,7 @@ export default function ProjectsList({ projects }) {
             })}
           </div>
         )}
-        {filteredProjects.length == 0 && <p>No projects to show</p>}
+        {projects.length == 0 && <p>No projects to show</p>}
       </div>
     </>
   );
